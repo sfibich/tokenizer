b7848d276e2cb983e67ebeeb5ff481bea67e0626
Steve Fibich <stevefibich@Steves-MacBook-Air.local>
2021-09-20 20:41:55 -0400

    almost there only one failure left

--- Tokenizer/Tokenize/token.py
+++ Tokenizer/Tokenize/token.py
@@ -1,14 +1,49 @@
 import random
+import hashlib
+import json
 from string import ascii_letters
 from string import digits
 
 class Token():
-    
+    tokens = {}
+
     def __init__(self,value):
         self.raw_value=value
-        self.token_value = self.get_token()
+        self.key = self.get_key()
+        self.token = self.get_token_from_store()
+        self.token_value = self.get_token_value()
+        if (self.token is None):
+            self.token_value = self.get_token_value()
+            self.token = self.write_token()
+            self.write_token_to_store()
+        #    #put into store
+        else:
+            pass
+        #    #self.token_value = self.token["token_value"]
 
-    def get_token(self):
+    def write_token_to_store(self):
+        Token.tokens[self.key] = self.token
+
+    def write_token(self):
+        token = {
+                "token_value": self.token_value,
+                "raw_value":self.raw_value,
+                }
+        return json.dumps(token)
+
+    def get_token_from_store(self):
+        try:
+            token = Token.tokens[self.key]
+        except KeyError:
+            token = None
+        return token
+
+    def get_key(self):
+        m = hashlib.sha256()
+        m.update(self.raw_value.encode())
+        return m.hexdigest()
+
+    def get_token_value(self):
         '''
         Get length of raw value
         if numeric only ; give numeric  token back
