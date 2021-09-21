b20e8306033cf6ba9478832b5350cad90742172c
Steve Fibich <steve.fibich@nowhere.com>
2021-09-21 12:26:52 -0400

    function working...needs backend

--- Tokenizer/Tokenize/__init__.py
+++ Tokenizer/Tokenize/__init__.py
@@ -1,22 +1,23 @@
 import logging
 
 import azure.functions as func
-import token
+from Tokenize.token import Token
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    value = req.params.get('value')
+    if not value:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            value = req_body.get('value')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if value:
+        t = Token(value)
+        return func.HttpResponse(t.token)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
